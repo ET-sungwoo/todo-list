33c8ba39b1951a36ad956c66114222555a891ad1
sungwoo <130328972+ET-sungwoo@users.noreply.github.com>
2023-12-27 10:18:16 +0900

    formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,91 +1,100 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
 import TodoList from './components/TodoList';
-import {v4 as uuidv4} from 'uuid';
-import {isEmpty, trim, sortBy} from "lodash-es";
+import { v4 as uuidv4 } from 'uuid';
+import { isEmpty, trim, sortBy } from 'lodash-es';
 
 function App() {
+  const [todoText, setTodoText] = useState('');
+  const [todos, setTodos] = useState([]);
 
-    const [todoText, setTodoText] = useState('');
-    const [todos, setTodos] = useState([]);
+  const onTodoTextChangeHandler = (event) => {
+    setTodoText(event.target.value);
+  };
 
-    const onTodoTextChangeHandler = (event) => {
-        setTodoText(event.target.value);
-    };
-
-    const onEnterPressedHandler = (event) => {
-        if (event.key === 'Enter') {
-            appendTodos(event);
-        }
-    };
-
-    const appendTodos = () => {
-        if (!isEmpty(trim(todoText))) {
-            const now = new Date();
-            const appendedTodos = sortBy([...todos, {
-                id: uuidv4(),
-                text: todoText,
-                completed: false,
-                createdAt: now,
-                updatedAt: now,
-            }], 'createdAt');
-            setTodos(appendedTodos.reverse());
-            setTodoText('');
-        }
-    };
-
-    const removeTodoById = (id) => {
-            setTodos((oldTodos) => {
-                return (oldTodos || []).filter(todo => todo.id !== id);
-            });
-    };
-
-    const updateTodoById = (id, todoObj) => {
-            setTodos((oldTodos) => {
-                return (oldTodos || []).map(todo => {
-                    if (todo.id === id) {
-                        return {
-                            ...todo,
-                            ...todoObj,
-                            id: todo.id,
-                            updatedAt: new Date(),
-                        };
-                    }
-                    return todo;
-                });
-            });
+  const onEnterPressedHandler = (event) => {
+    if (event.key === 'Enter') {
+      appendTodos(event);
     }
+  };
 
-    return (
-        <div className="flex flex-col w-full border-opacity-50">
-            <div className="flex px-5 py-5">
-                <div className="flex flex-col space-y-2">
-                    <h2 className="text-gray-70 text-xl font-bold">
-                        Todo List
-                    </h2>
-                    {/*<p>Join 5,000+ others and never miss out on new tips, tutorials, and more.</p>*/}
-                    <div className="flex flex-col gap-3 md:flex-row">
-                        <div className="form-control flex-grow">
-                            <input
-                                id="iptTodoValue"
-                                type="text"
-                                name="todoValue"
-                                placeholder="Type here"
-                                className="input input-bordered flex-grow"
-                                value={todoText}
-                                onChange={onTodoTextChangeHandler}
-                                onKeyUp={onEnterPressedHandler}
-                            />
-                        </div>
-                        <button className="btn btn-primary" onClick={appendTodos}>Add</button>
-                    </div>
-                </div>
+  const appendTodos = () => {
+    if (!isEmpty(trim(todoText))) {
+      const now = new Date();
+      const appendedTodos = sortBy(
+        [
+          ...todos,
+          {
+            id: uuidv4(),
+            text: todoText,
+            completed: false,
+            createdAt: now,
+            updatedAt: now,
+          },
+        ],
+        'createdAt'
+      );
+      setTodos(appendedTodos.reverse());
+      setTodoText('');
+    }
+  };
+
+  const removeTodoById = (id) => {
+    setTodos((oldTodos) => {
+      return (oldTodos || []).filter((todo) => todo.id !== id);
+    });
+  };
+
+  const updateTodoById = (id, todoObj) => {
+    setTodos((oldTodos) => {
+      return (oldTodos || []).map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            ...todoObj,
+            id: todo.id,
+            updatedAt: new Date(),
+          };
+        }
+        return todo;
+      });
+    });
+  };
+
+  return (
+    <div className="flex flex-col w-full border-opacity-50">
+      <div className="flex px-5 py-5">
+        <div className="flex flex-col space-y-2">
+          <h2 className="text-gray-70 text-xl font-bold">Todo List</h2>
+          {/*<p>Join 5,000+ others and never miss out on new tips, tutorials, and more.</p>*/}
+          <div className="flex flex-col gap-3 md:flex-row">
+            <div className="form-control flex-grow">
+              <input
+                id="iptTodoValue"
+                type="text"
+                name="todoValue"
+                placeholder="Type here"
+                className="input input-bordered flex-grow"
+                value={todoText}
+                onChange={onTodoTextChangeHandler}
+                onKeyUp={onEnterPressedHandler}
+              />
             </div>
-            <div className="divider" />
-            {/* Todos */}
-            <TodoList todos={todos} removeHandler={removeTodoById} updateHandler={updateTodoById} />
+            <button className="btn btn-primary" onClick={appendTodos}>
+              Add
+            </button>
+          </div>
         </div>
-    );
+      </div>
+      <div className="divider" />
+      {/* Todos */}
+      <TodoList
+        todos={todos}
+        removeHandler={removeTodoById}
+        updateHandler={updateTodoById}
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,23 +1,20 @@
-import TodoItem from "./TodoItem";
+import TodoItem from './TodoItem';
 
-function TodoList({todos = [], removeHandler, updateHandler}) {
-
-    return (
-        <div className="flex flex-col items-center justify-center w-96">
-            <ul className="todo-list">
-                {
-                    todos.map((todo) => (
-                        <TodoItem
-                            key={todo.id}
-                            todo={todo}
-                            removeHandler={removeHandler}
-                            updateHandler={updateHandler}
-                        />
-                    ))
-                }
-            </ul>
-        </div>
-    );
+function TodoList({ todos = [], removeHandler, updateHandler }) {
+  return (
+    <div className="flex flex-col items-center justify-center w-96">
+      <ul className="todo-list">
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            removeHandler={removeHandler}
+            updateHandler={updateHandler}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default TodoList;
